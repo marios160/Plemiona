91821443d92198c29e4069bddab8c89d19995a9a
Mateusz Błaszczak <marios160@o2.pl>
2020-12-15 17:55:13 +0100

    Poprawki w skrypcie do pobierania danych o splądrowanych surowcach. Zabezpieczono skrypt na wypadek nickow ze spacjami

--- public_html/getPlunderedResources.js
+++ public_html/getPlunderedResources.js
@@ -1,49 +1,54 @@
-//pobierz danie o surowcach
 javascript:{
-    var stats = sessionStorage.getItem('stats');
-    if (stats == null) {
-        var list = prompt('Wklej liste graczy');
-        listSplit = list.split(' ');
-        var arr = [];
-        for (var i = 6; i < listSplit.length; i += 5) {
-            arr.push(listSplit[i]);
-        }
-        var nicks = JSON.stringify(arr.reverse());
-        sessionStorage.setItem('players', nicks);
-        sessionStorage.setItem('stats', '');
-    } else {
-        var row = document.getElementsByClassName('lit');
-        if (row.length == 0) {
-            row = document.getElementsByClassName('lit2');
-            if (row.length == 0)
-                row = [];
-            else
-                row = row[0].children;
-        } else {
-            row = row[0].children;
-        }
-        var copy = "";
-        for (var i = 1; i < row.length; i++) {
-            if (i == 3) {
-                copy = copy + row[i].innerText.replace(".", "") + " \t";
-            } else {
-                copy = copy + row[i].innerText + " \t";
-            }
-        };
-        copy = copy.substring(0, copy.length - 2);
-        sessionStorage.setItem('stats', stats + copy + '\r\n');
-    }
-    var players = JSON.parse(sessionStorage.getItem('players'));
-    var input = document.getElementsByName('name');
-    if (players.length == 0) {
-        alert(sessionStorage.getItem('stats'));
+    if (!window.location.href.includes("in_a_day")) {
         sessionStorage.removeItem('stats');
-        sessionStorage.removeItem('players');
+        sessionStorage.removeItem('plazers');
+        window.location.href = "https://pl160.plemiona.pl/game.php?screen=ranking&mode=in_a_day";
     } else {
-        input[0].value = players[players.length - 1];
-        players.pop();
-        sessionStorage.setItem('players', JSON.stringify(players));
-        input[0].nextElementSibling.click();
-    }
 
+        var stats = sessionStorage.getItem('stats');
+        if (stats == null) {
+            var list = prompt('Wklej liste graczy');
+            list = list.replace('Nazwa \tRanking \tP. \tRanking globalny \tWioski ', '');
+            listSplit = list.split(/ \t\d{0,2} \t\d{1,},{0,1}\d{0,3} \t\d{1,} \t\d{1,}/);
+            console.log(listSplit);
+            var arr = [];
+            for (var i = 0; i < listSplit.length; i++) {
+                if (listSplit[i].length > 0) {
+                    var p = listSplit[i].replace(/\(.*\)/, '');
+                    arr.push(p);
+                }
+            }
+            var nicks = JSON.stringify(arr.reverse());
+            sessionStorage.setItem('players', nicks);
+            sessionStorage.setItem('stats', '');
+        } else {
+            var row = document.getElementsByClassName('lit-item');
+            var copy = "";
+            for (var i = 1; i < row.length; i++) {
+                if (i == 2)
+                    continue;
+                if (i == 3) {
+                    copy = copy + row[i].innerText.replace(".", "") + " \t";
+                } else {
+                    copy = copy + row[i].innerText + " \t";
+                }
+            }
+            ;
+            copy = copy.substring(0, copy.length - 2);
+            sessionStorage.setItem('stats', stats + copy + '\r\n');
+        }
+        var players = JSON.parse(sessionStorage.getItem('players'));
+        var input = document.getElementsByName('name');
+        if (players.length == 0) {
+            alert(sessionStorage.getItem('stats'));
+            sessionStorage.removeItem('stats');
+            sessionStorage.removeItem('players');
+        } else {
+            var player = players[players.length - 1].trim();
+            players.pop();
+            sessionStorage.setItem('players', JSON.stringify(players));
+            window.location.href = "https://pl160.plemiona.pl/game.php?screen=ranking&mode=in_a_day&type=loot_res&name=" + player;
+        }
+
+    }
 }
